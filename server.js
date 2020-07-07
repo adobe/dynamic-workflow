c9d37e7583baa8edece71ee8d1769cd1d8e1db82
Travis Krause <tkrause@adobe.com>
2020-07-07 13:55:34 -0500

    Linting added & minor header fix

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ governing permissions and limitations under the License.
 
 // Express, Async, Fetch, & Body Parser
 const express = require('express');
-const async = require('express-async-await');
 const fetch = require('node-fetch');
 const sleep = require('await-sleep');
 
@@ -35,12 +34,19 @@ const config = yaml.safeLoad(fs.readFileSync(path.join(__dirname, 'config', 'con
 const app = express();
 
 // Configuration
-var integration = config['enterprise']['integration'];
 var host = config['server']['host'];
 var endpoint = config['server']['endpoint'];
 var url = host + endpoint;
 var port = process.env.PORT || config.port || 80;
-var x_api_user = config['features']['x-api-user'];
+
+var headers = {
+  'Access-Token': config['enterprise']['integration'],
+  'Accept': 'application/json',
+  'Content-Type': 'application/json'
+};
+if (config['features']['x-api-user']) {
+  headers['x-api-user'] = config['features']['x-api-user'];
+}
 
 
 app.use(express.static(__dirname + '/static'));
@@ -51,184 +57,140 @@ app.use(bodyParser.json());
 
 // Get index.html page from server
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/static/test.html');
+  res.sendFile(__dirname + '/static/test.html');
 });
 
 // Get features from config files
 app.get('/features', function (req, res){
-    res.json(config['features'])
-})
+  res.json(config['features']);
+});
 
 // GET /workflows
-app.get('/api/getWorkflows', async function (req, res, next) {
+app.get('/api/getWorkflows', async function (req, res) {
 
-    function getWorkflows() {
-        /***
+  function getWorkflows() {
+    /***
          * This function makes a request to get workflows
          */
-        const endpoint = '/workflows';
-        const headers = {
-            'Access-Token': integration,
-            'x-api-user': x_api_user
-        };
+    const endpoint = '/workflows';
 
-        return fetch(url + endpoint, {
-            method: 'GET',
-            headers: headers});
-    }
+    return fetch(url + endpoint, {
+      method: 'GET',
+      headers: headers});
+  }
 
-    const workflow_list = await getWorkflows();
-    const data = await workflow_list.json();
+  const workflow_list = await getWorkflows();
+  const data = await workflow_list.json();
 
-    res.json(data['userWorkflowList']);
+  res.json(data['userWorkflowList']);
 });
 
 // GET /workflows/{workflowId}
-app.get('/api/getWorkflowById/:id', async function(req, res, next){
+app.get('/api/getWorkflowById/:id', async function(req, res){
 
-    function getWorkflowById() {
-        /***
+  function getWorkflowById() {
+    /***
          * This function makes a request to get workflow by ID
          */
-        const endpoint = "/workflows/" + req.params.id;
-        const headers = {
-            'Access-Token': integration,
-            'x-api-user': x_api_user
-        };
+    const endpoint = '/workflows/' + req.params.id;
 
-        return fetch(url + endpoint, {
-            method: 'GET',
-            headers: headers})
-    }
+    return fetch(url + endpoint, {
+      method: 'GET',
+      headers: headers});
+  }
 
-    const workflow_by_id = await getWorkflowById();
-    const data = await workflow_by_id.json();
+  const workflow_by_id = await getWorkflowById();
+  const data = await workflow_by_id.json();
 
-    res.json(data);
-});
-
-// GET /libraryDocuments/{libraryDocumentId}/documents
-app.get('/api/getLibraryDocuments/:id', async function(req, res, next) {
-
-    function getLibraryDocuments() {
-        /***
-         * This function gets library documents by ID
-         */
-        const endpoint = "/libraryDocuments/" + req.params.id + "/documents";
-        const headers = {
-            'Access-Token': integration,
-            'x-api-user': x_api_user
-        };
-
-        return fetch(url + endpoint, {method: 'GET', headers: headers})
-    }
-
-    const library_document = await getLibraryDocuments();
-    const data = await library_document.json();
-
-    es.json(data);
+  res.json(data);
 });
 
 // POST /workflows/{workflowId}/agreements
-app.post('/api/postAgreement/:id', async function(req, res, next){
+app.post('/api/postAgreement/:id', async function(req, res){
 
-    function postAgreement() {
-        /***
+  function postAgreement() {
+    /***
          * This function post agreements
          */
-        const endpoint = "/workflows/" + req.params.id + "/agreements";
-        const headers = {
-            'Access-Token': integration,
-            'x-api-user': x_api_user,
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-        };
+    const endpoint = '/workflows/' + req.params.id + '/agreements';
 
-        return fetch(url + endpoint, {
-            method:'POST',
-            headers: headers,
-            body: JSON.stringify(req.body)})
-    }
+    return fetch(url + endpoint, {
+      method:'POST',
+      headers: headers,
+      body: JSON.stringify(req.body)});
+  }
 
-    const api_response = await postAgreement();
-    const data = await api_response.json();
+  const api_response = await postAgreement();
+  const data = await api_response.json();
 
-    res.json(data);
+  res.json(data);
 });
 
 
 // GET /agreements/{agreementId}/signingUrls
-app.get('/api/getSigningUrls/:id', async function(req, res, next){
+app.get('/api/getSigningUrls/:id', async function(req, res){
 
-    async function getSigningUrls(count=0) {
-        /***
-         * This function gets URL for the e-sign page for the current signer(s) of an agreement.
-         */
-        const endpoint = "/agreements/" + req.params.id + "/signingUrls";
-        const headers = {
-            'Access-Token': integration,
-            'x-api-user': x_api_user
-        };
+  async function getSigningUrls(count=0) {
+    /***
+     * This function gets URL for the e-sign page for the current signer(s) of an agreement.
+     */
+    const endpoint = '/agreements/' + req.params.id + '/signingUrls';
 
-        const sign_in_response = await fetch(url + endpoint, {
-            method:'GET',
-            headers: headers})             
-             
-        const sign_in_data =  await sign_in_response.json();
-        if(sign_in_data.code === 'AGREEMENT_NOT_SIGNABLE'){
-                 //retry for 5 times with 2000ms delay
-                if(count >= 5){
-                     return sign_in_data;
-                }else{
-                     await sleep(2000);
-                     count++;
-                     return await getSigningUrls(count);
-                }
-        }
+    const sign_in_response = await fetch(url + endpoint, {
+      method:'GET',
+      headers: headers});
+
+    const sign_in_data =  await sign_in_response.json();
+    if(sign_in_data.code === 'AGREEMENT_NOT_SIGNABLE'){
+      //retry for 5 times with 2000ms delay
+      if(count >= 5){
         return sign_in_data;
+      }else{
+        await sleep(2000);
+        count++;
+        return await getSigningUrls(count);
+      }
     }
+    return sign_in_data;
+  }
 
-    const data = await getSigningUrls();
-    //const data =  await api_response.json();
+  const data = await getSigningUrls();
+  //const data =  await api_response.json();
 
-    res.json(data);
+  res.json(data);
 });
 
 // POST /transientDocuments
-app.post('/api/postTransient', upload.single('myfile'), async function (req, res, next) {
+app.post('/api/postTransient', upload.single('myfile'), async function (req, res) {
 
-    function postTransient() {
-        /***
+  function postTransient() {
+    /***
          * This functions post transient
          */
-        const endpoint = "/transientDocuments";
-        const headers = {
-            'Access-Token': integration,
-            'x-api-user': x_api_user
-        };
+    const endpoint = '/transientDocuments';
 
-        return fetch(url + endpoint, {
-            method: 'POST',
-            headers: headers,
-            body: form
-        })
-    }
-
-    // Create FormData
-    var form = new FormData();
-    form.append('File-Name', req.file.originalname);
-    form.append('Mime-Type', req.file.mimetype);
-    form.append('File', fs.createReadStream(req.file.path));
-
-    const api_response = await postTransient();
-    const data = await api_response.json();
-
-    // Delete uploaded doc after transient call
-    fs.unlink(req.file.path, function (err) {
-        if (err) return console.log(err);
+    return fetch(url + endpoint, {
+      method: 'POST',
+      headers: headers,
+      body: form
     });
+  }
 
-    res.json(data)
-  })
+  // Create FormData
+  var form = new FormData();
+  form.append('File-Name', req.file.originalname);
+  form.append('Mime-Type', req.file.mimetype);
+  form.append('File', fs.createReadStream(req.file.path));
+
+  const api_response = await postTransient();
+  const data = await api_response.json();
+
+  // Delete uploaded doc after transient call
+  fs.unlink(req.file.path, function (err) {
+    if (err) return console.log(err);
+  });
+
+  res.json(data);
+});
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
